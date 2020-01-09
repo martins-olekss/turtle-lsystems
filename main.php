aefd9040571572756e925f23a4a276836146d8af
Martins <olekss.martins@gmail.com>
2020-01-09 12:35:28 +0200

    Major refactor, make separate class for logic

--- main.php
+++ main.php
@@ -1,59 +1,20 @@
 <?php
-require 'src/phpturtle/turtle.php';
-const IMAGE_WIDTH = 2300;
-const IMAGE_HEIGHT = 2300;
-const MOVE_DISTANCE = 40;
-const ROTATE_ANGLE = 33.5;
-const START_POSITION = [600, 600];
-const START_STRING = '+F-F+YX-F+F-X';
-const ITERATIONS = 130;
-const RULES = [
-    ['X', '+YF−XFX−FY+'],
-    ['Y', '−XF+YFY+FX−']
+require_once 'src/Lsystems.php';
+
+$ls = new Lsystems();
+$settings = [
+    'imageWidth' => 2200,
+    'imageHeight' => 2200,
+    'startPosition' => [600, 600],
+    'iterations' => 130,
+    'startInput' => '+F-F+YX-F+F-X',
+    'rotate' => 33.5,
+    'move' => 40,
+    'rules' => [
+        ['X', '+YF−XFX−FY+'],
+        ['Y', '−XF+YFY+FX−']
+    ]
 ];
 
-$im = new Image(IMAGE_WIDTH, IMAGE_HEIGHT, 0,0,0);
-$turtle = new Turtle($im);
-//$turtle->font = 'FreeMono.ttf';
-
-function lsystem($string) {
-    foreach (RULES as $rule) {
-        $string = str_replace($rule[0], $rule[1], $string);
-    }
-
-    return $string;
-}
-
-function translate($inputArray, Turtle $turtle) {
-    foreach ($inputArray as $char) {
-        switch ($char) {
-            case 'F':
-                $turtle->move(MOVE_DISTANCE);
-                break;
-            case '-':
-                $turtle->set_color(250, 150, 19, 10);
-                $turtle->rotate(- ROTATE_ANGLE);
-                break;
-            case '+':
-                $turtle->set_color(20, 240, 90, 100);
-                $turtle->rotate(ROTATE_ANGLE);
-                break;
-        }
-    }
-}
-
-function drawLsystem($input, Turtle $turtle, Image $im) {
-    $inputArray = str_split($input);
-    $turtle->set_color(255, 128, 64, 0);
-    $turtle->goto(20, 20);
-    $turtle->goto(START_POSITION[0], START_POSITION[1]);
-    $turtle->set_color(200, 5, 9, 20);
-    for($i=0; $i <= ITERATIONS; $i ++) {
-        translate($inputArray, $turtle);
-    }
-
-    $im->send_gif();
-}
-
-$output = lsystem(START_STRING);
-drawLsystem($output, $turtle, $im);
+$ls->settings($settings);
+$ls->execute();
---
+++ src/Lsystems.php
@@ -0,0 +1,90 @@
+<?php
+require_once 'phpturtle/turtle.php';
+
+class Lsystems
+{
+    public $settings = [
+        'imageWidth' => 1200,
+        'imageHeight' => 1200,
+        'startPosition' => [300, 300],
+        'iterations' => 100,
+        'startInput' => '+F-F+YX-F+F-X',
+        'rotate' => 30,
+        'move' => 20,
+        'rules' => [
+            ['X', '+YF−XFX−FY+'],
+            ['Y', '−XF+YFY+FX−']
+        ]
+    ];
+    public $image;
+    public $turtle;
+    public $string = [];
+
+    public function __construct()
+    {
+        $this->image = new Image($this->settings['imageWidth'], $this->settings['imageHeight'], 0, 0, 0);
+        $this->turtle = new Turtle($this->image);
+    }
+
+    public function settings(array $settings) {
+        $this->settings = $settings;
+    }
+
+    public function execute()
+    {
+        $this->string[] = $this->morph($this->settings['startInput']);
+        $this->draw($this->string[0]);
+        $this->image->send_gif();
+    }
+
+    /**
+     * @param $string
+     * @return mixed
+     */
+    public function morph($string)
+    {
+        // TODO: Add ability to have multiple morph iterations
+        foreach ($this->settings['rules'] as $rule) {
+            $string = str_replace($rule[0], $rule[1], $string);
+        }
+
+        return $string;
+    }
+
+    /**
+     * @param array $input
+     */
+    public function operateTurtle(array $input)
+    {
+        foreach ($input as $char) {
+            switch ($char) {
+                case 'F':
+                    $this->turtle->move($this->settings['move']);
+                    break;
+                case '-':
+                    $this->turtle->set_color(250, 150, 19, 10);
+                    $this->turtle->rotate(-$this->settings['rotate']);
+                    break;
+                case '+':
+                    $this->turtle->set_color(20, 240, 90, 100);
+                    $this->turtle->rotate($this->settings['rotate']);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @param $input
+     */
+    public function draw($input)
+    {
+        $inputArray = str_split($input);
+        $this->turtle->set_color(255, 128, 64, 0);
+        $this->turtle->goto(20, 20);
+        $this->turtle->goto($this->settings['startPosition'][0], $this->settings['startPosition'][1]);
+        $this->turtle->set_color(200, 5, 9, 20);
+        for ($i = 0; $i <= $this->settings['iterations']; $i++) {
+            $this->operateTurtle($inputArray);
+        }
+    }
+}
