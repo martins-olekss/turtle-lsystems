1cf818a25591ccde94d57f1b7047aae25293cee4
Martins <olekss.martins@gmail.com>
2020-01-12 13:03:28 +0200

    Add function to add rules via form Install jquery Create js functions for rule editing

--- form.php
+++ form.php
@@ -10,35 +10,54 @@ $settings =
         'rotate' => 90,
         'move' => 30,
         'rules' => [
-            ['X','+YF-XFX-FY+'],
-            ['Y','-YF+XFX+FY-']
+            ['X', '+YF-XFX-FY+'],
+            ['Y', '-YF+XFX+FY-']
         ]
     ];
 ?>
-<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
     <title>Generate images</title>
     <link rel="stylesheet" href="css/main.css">
 
 </head>
 <body>
-    <div class="wrapper">
-        <form method="get" action="draw.php">
-            <label>imageWidth: <input type="text" name="imageWidth" value="<?= $settings['imageWidth'] ?>"/></label>
-            <label>imageHeight: <input type="text" name="imageHeight" value="<?= $settings['imageHeight'] ?>"/></label>
-            <label>startPositionX: <input type="text" name="startPositionX" value="<?= $settings['startPosition'][0] ?>"/></label>
-            <label>startPositionY: <input type="text" name="startPositionY" value="<?= $settings['startPosition'][1] ?>"/></label>
-            <label>iterations: <input type="text" name="iterations" value="<?= $settings['iterations'] ?>"/></label>
-            <label>startInput: <input type="text" name="startInput" value="<?= $settings['startInput'] ?>" /></label>
-            <label>rotate: <input type="text" name="rotate" value="<?= $settings['rotate'] ?>"/></label>
-            <label>move: <input type="text" name="move" value="<?= $settings['move'] ?>"/></label>
-            <label>rules:
-                <textarea name="rules"><?= json_encode($settings['rules']) ?></textarea>
+<div class="wrapper">
+    <form method="get" action="draw.php">
+        <label>imageWidth: <input type="text" name="imageWidth" value="<?= $settings['imageWidth'] ?>"/></label>
+        <label>imageHeight: <input type="text" name="imageHeight" value="<?= $settings['imageHeight'] ?>"/></label>
+        <label>startPositionX: <input type="text" name="startPositionX"
+                                      value="<?= $settings['startPosition'][0] ?>"/></label>
+        <label>startPositionY: <input type="text" name="startPositionY"
+                                      value="<?= $settings['startPosition'][1] ?>"/></label>
+        <label>iterations: <input type="text" name="iterations" value="<?= $settings['iterations'] ?>"/></label>
+        <label>startInput: <input type="text" name="startInput" value="<?= $settings['startInput'] ?>"/></label>
+        <label>rotate: <input type="text" name="rotate" value="<?= $settings['rotate'] ?>"/></label>
+        <label>move: <input type="text" name="move" value="<?= $settings['move'] ?>"/></label>
+        <div style="display: block;">
+            <label> Add rule:
+                <input type="text" class="first-part" name="first_part">
+                <input type="text" class="second-part" name="second_part">
             </label>
-            <input type="submit">
-        </form>
-    </div>
+            <button type="button" class="add-button">add</button>
+            <button type="button" class="refresh-button">refresh</button>
+            <button type="button" class="update-button">update array from input</button>
+        </div>
+
+        <label>rules:
+            <textarea class="rules" name="rules"><?= json_encode($settings['rules']) ?></textarea>
+        </label>
+        <input type="submit">
+    </form>
+
+
+</div>
+<div class="rules-list"></div>
+
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript">
+    window.rulesJson = JSON.parse($('.rules').val());
+</script>
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,35 @@
+refreshRules = function () {
+    $('.rules-list').empty();
+    $.each(rulesJson, function (k, v) {
+        $(".rules-list").append('<p>[' + k + '] ' + v + ' <button type="button" data-id="' + k + '" onclick="removeRule(this)"> remove </button></p>');
+    });
+    updateRules();
+};
+
+removeRule = function (el) {
+    let id = $(el).data('id');
+    rulesJson.splice(id, 1);
+    refreshRules();
+};
+
+updateRules = function () {
+    let rules = $('.rules');
+    rules.val(JSON.stringify(rulesJson));
+};
+
+$('.update-button').click(function () {
+    rulesJson = JSON.parse($('.rules').val());
+    refreshRules();
+});
+
+$('.refresh-button').click(function () {
+    refreshRules();
+});
+
+refreshRules();
+$('.add-button').click(function () {
+    let first = $('.first-part').val();
+    let second = $('.second-part').val();
+    rulesJson.push([first, second]);
+    refreshRules();
+});
