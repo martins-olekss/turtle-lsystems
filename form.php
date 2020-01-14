1decffce8bd2a7fdecf5f175cfc39261e0ef55bd
Martins <olekss.martins@gmail.com>
2020-01-14 14:44:25 +0200

    Rework input form Add vue, add vue logic for rule inputs

--- form.php
+++ form.php
@@ -10,56 +10,82 @@ $settings =
         'rotate' => 22,
         'move' => 30,
         'rules' => [
-            ['X','+YF-XFX-FY+'],
-            ['Y','-YF+XFY-'],
-            ['-X','+YF-XY+'],
-            ['+Y','-YFX+FY-'],
+            ['X', '+YF-XFX-FY+'],
+            ['Y', '-YF+XFY-'],
+            ['-X', '+YF-XY+'],
+            ['+Y', '-YFX+FY-'],
         ]
     ];
 ?>
-<html lang="en">
+<html lang="en" xmlns:v-on="http://www.w3.org/1999/xhtml">
 <head>
     <title>Generate images</title>
     <link rel="stylesheet" href="css/main.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">
 </head>
 <body>
-<div class="wrapper">
+<div id="app">
     <form method="get" action="draw.php">
-        <label>imageWidth: <input type="text" name="imageWidth" value="<?= $settings['imageWidth'] ?>"/></label>
-        <label>imageHeight: <input type="text" name="imageHeight" value="<?= $settings['imageHeight'] ?>"/></label>
-        <label>startPositionX: <input type="text" name="startPositionX"
-                                      value="<?= $settings['startPosition'][0] ?>"/></label>
-        <label>startPositionY: <input type="text" name="startPositionY"
-                                      value="<?= $settings['startPosition'][1] ?>"/></label>
+        <label>width, height:
+            <input type="text" name="imageWidth" value="<?= $settings['imageWidth'] ?>"/>
+            <input type="text" name="imageHeight" value="<?= $settings['imageHeight'] ?>"/>
+        </label>
+        <label>drawing start x, y: <input type="text" name="startPositionX"
+                                          value="<?= $settings['startPosition'][0] ?>"/>
+            <input type="text" name="startPositionY"
+                   value="<?= $settings['startPosition'][1] ?>"/></label>
         <label>iterations: <input type="text" name="iterations" value="<?= $settings['iterations'] ?>"/></label>
         <label>startInput: <input type="text" name="startInput" value="<?= $settings['startInput'] ?>"/></label>
         <label>rotate: <input type="text" name="rotate" value="<?= $settings['rotate'] ?>"/></label>
         <label>move: <input type="text" name="move" value="<?= $settings['move'] ?>"/></label>
-        <div style="display: block;">
-            <label> Add rule:
-                <input type="text" class="first-part" name="first_part">
-                <input type="text" class="second-part" name="second_part">
-            </label>
-            <button type="button" class="add-button">add</button>
-            <button type="button" class="refresh-button">refresh</button>
-            <button type="button" class="update-button">update array from input</button>
-        </div>
-
-        <label>rules:
-            <textarea class="rules" name="rules"><?= json_encode($settings['rules']) ?></textarea>
-        </label>
-        <input type="submit">
+        <table>
+            <tr>
+                <th>id</th>
+                <th>subject</th>
+                <th>replacement</th>
+                <th>action</th>
+            </tr>
+            <tr class="new-rule">
+                <td></td>
+                <td><input type="text" v-model="newObject" value="1"/></td>
+                <td><input type="text" v-model="newReplacement" value="2"/></td>
+                <td>
+                    <button type="button" v-on:click="add(newObject, newReplacement)">add</button>
+                </td>
+            </tr>
+            <tr v-for="(item, index) in settings.rules">
+                <td>{{ index }}</td>
+                <td><input v-model="settings.rules[index][0]" type="text"/></td>
+                <td><input v-model="settings.rules[index][1]" type="text"/></td>
+                <td>
+                    <button type="button" v-on:click="remove(index)">remove</button>
+                </td>
+            </tr>
+        </table>
+        <textarea style="display: none" class="rules" name="rules" title="draw rules">{{ settings.rules }}</textarea>
+        <input type="submit" class="submit" name="submitSettings" value="Start drawing process">
     </form>
-
-
 </div>
-<div class="rules-list"></div>
 
-<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/vue.min.js"></script>
 <script type="text/javascript">
-    window.rulesJson = JSON.parse($('.rules').val());
+    new Vue({
+        el: '#app',
+        data: {
+            settings: <?= json_encode($settings) ?>,
+            newObject: '',
+            newReplacement: '',
+        },
+        methods: {
+            remove: function (id) {
+                this.settings.rules.splice(id, 1);
+            },
+            add: function (object, replacement) {
+                console.log(object + ' - ' + replacement);
+                this.settings.rules.push([object, replacement]);
+            }
+        }
+    });
 </script>
-<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
